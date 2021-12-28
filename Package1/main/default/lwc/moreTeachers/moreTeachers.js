055160679326c6830d760925e7d361851e6d3c9f
Rebecca <rebeccahahn40@gmail.com>
2021-12-28 14:34:08 -0500

    push5

--- Package1/main/default/lwc/moreTeachers/moreTeachers.js
+++ Package1/main/default/lwc/moreTeachers/moreTeachers.js
@@ -5,12 +5,12 @@ export default class moreTeachers extends LightningElement {
     showTeachers = true;
 
     myTeachers = [
-        { name: "Elsie Foley", image: "../../../resources/teacher4.png" },
-        { name: "Jaime Kendall", image: "../../../resources/teacher5.png" },
-        { name: "Margie Lowe", image: "../../../resources/teacher6.png"},
-        { name: "Ewan Phillips", image: "../../../resources/teacher7.png" },
-        { name: "Marrisa Burton", image: "../../../resources/teacher8.png" },
-        { name: "Britany O'Doherty", image: "../../../resources/teacher9.png"}
+        { name: "Elsie Foley", image: "../../../../../images/teacher4.png" },
+        { name: "Jaime Kendall", image: "../../../../../images/teacher5.png" },
+        { name: "Margie Lowe", image: "../../../../../images/teacher6.png"},
+        { name: "Ewan Phillips", image: "../../../../../images/teacher7.png" },
+        { name: "Marrisa Burton", image: "../../../../../images/teacher8.png" },
+        { name: "Britany O'Doherty", image: "../../../../../images/teacher9.png"}
     ];
 
 }
--- Package1/main/default/lwc/teachers/teachers.js
+++ Package1/main/default/lwc/teachers/teachers.js
@@ -5,9 +5,9 @@ export default class teachers extends LightningElement {
     showTeachers = true;
 
     myTeachers = [
-        { name: "William Bermudez", image: "../../../resources/teacher1.png" },
-        { name: "Suzanna Gianattasio", image: "../../../resources/teacher2.png" },
-        { name: "Antonia Shachar", image: "../../../resources/teacher3.png"}
+        { name: "William Bermudez", image: "../../../../../images/teacher1.png" },
+        { name: "Suzanna Gianattasio", image: "../../../../../images/teacher2.png" },
+        { name: "Antonia Shachar", image: "../../../../../images/teacher3.png"}
     ];
 
 }
