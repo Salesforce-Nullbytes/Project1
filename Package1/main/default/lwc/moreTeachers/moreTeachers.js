00fba3a912bdf3a0f1f73771d16699a3b50df476
Rebecca <rebeccahahn40@gmail.com>
2021-12-29 12:04:30 -0500

    push

--- Package1/main/default/lwc/moreTeachers/moreTeachers.js
+++ Package1/main/default/lwc/moreTeachers/moreTeachers.js
@@ -1,16 +1,28 @@
 import {LightningElement} from 'lwc';
+import teacher4 from '@salesforce/resourceUrl/teacher4';
+import teacher5 from '@salesforce/resourceUrl/teacher5';
+import teacher6 from '@salesforce/resourceUrl/teacher6';
+import teacher7 from '@salesforce/resourceUrl/teacher7';
+import teacher8 from '@salesforce/resourceUrl/teacher8';
+import teacher9 from '@salesforce/resourceUrl/teacher9';
 
 export default class moreTeachers extends LightningElement {
+    teacher4 = teacher4;
+    teacher5 = teacher5;
+    teacher6 = teacher6;
+    teacher7 = teacher7;
+    teacher8 = teacher8;
+    teacher9 = teacher9;
 
     showTeachers = true;
 
     myTeachers = [
-        { name: "Elsie Foley", image: "../../../../../images/teacher4.png" },
-        { name: "Jaime Kendall", image: "../../../../../images/teacher5.png" },
-        { name: "Margie Lowe", image: "../../../../../images/teacher6.png"},
-        { name: "Ewan Phillips", image: "../../../../../images/teacher7.png" },
-        { name: "Marrisa Burton", image: "../../../../../images/teacher8.png" },
-        { name: "Britany O'Doherty", image: "../../../../../images/teacher9.png"}
+        { name: "Elsie Foley", image: teacher4 },
+        { name: "Jaime Kendall", image: teacher5 },
+        { name: "Margie Lowe", image: teacher6 },
+        { name: "Ewan Phillips", image: teacher7 },
+        { name: "Marrisa Burton", image: teacher8 },
+        { name: "Britany O'Doherty", image: teacher9 }
     ];
 
 }
--- Package1/main/default/lwc/teachers/teachers.js
+++ Package1/main/default/lwc/teachers/teachers.js
@@ -1,13 +1,19 @@
 import {LightningElement} from 'lwc';
+import teacher1 from '@salesforce/resourceUrl/teacher1';
+import teacher2 from '@salesforce/resourceUrl/teacher2';
+import teacher3 from '@salesforce/resourceUrl/teacher3';
 
 export default class teachers extends LightningElement {
+    teacher1 = teacher1;
+    teacher2 = teacher2;
+    teacher3 = teacher3;
 
     showTeachers = true;
 
     myTeachers = [
-        { name: "William Bermudez", image: "../../../../../images/teacher1.png" },
-        { name: "Suzanna Gianattasio", image: "../../../../../images/teacher2.png" },
-        { name: "Antonia Shachar", image: "../../../../../images/teacher3.png"}
+        { name: "William Bermudez", image: teacher1 },
+        { name: "Suzanna Gianattasio", image: teacher2 },
+        { name: "Antonia Shachar", image: teacher3 }
     ];
 
 }
