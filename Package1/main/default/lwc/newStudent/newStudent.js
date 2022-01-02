0367afd6fc831b7fdafb99cddf5af0a744717fee
Rebecca <rebeccahahn40@gmail.com>
2022-01-02 11:55:55 -0500

    push

--- Package1/main/default/lwc/newStudent/newStudent.js
+++ Package1/main/default/lwc/newStudent/newStudent.js
@@ -2,4 +2,11 @@ import {LightningElement} from 'lwc';
 
 export default class newStudent extends LightningElement {
 
+    handleStudentSubmit() {
+        let listOfStudentInfo = this.template.querySelectorAll("input[type=text]");
+
+        let newStudentName = listOfStudentInfo[0].value;
+        let newParentName = listOfStudentInfo[1].value;
+        let newParentEmail = listOfStudentInfo[2].value;
+    }
 }
--- Package1/main/default/lwc/newTeacher/newTeacher.js
+++ Package1/main/default/lwc/newTeacher/newTeacher.js
@@ -2,4 +2,14 @@ import {LightningElement} from 'lwc';
 
 export default class newTeacher extends LightningElement {
 
+    handleTeacherSubmit() {
+        let listOfTeacherInfo = this.template.querySelectorAll("input[type=text]");
+
+        let newTeacherName = listOfTeacherInfo[0].value;
+        let newTeacherEmail = listOfTeacherInfo[1].value;
+
+        console.log(newTeacherName);
+        console.log(newTeacherEmail);
+    }
+
 }
